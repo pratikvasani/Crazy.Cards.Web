515f62abe373d98ded4ef3834b67d3ae7253f7ba
pnvpratik <pnvpratik@gmail.com>
2016-03-19 18:03:58 +0000

    cards form working now.

--- Crazy.Cards.Web/Angular/Module/crazyCards.js
+++ Crazy.Cards.Web/Angular/Module/crazyCards.js
@@ -1,6 +1,6 @@
-﻿var module = angular.module("crazyCards", ['ui.router']);
+﻿var app = angular.module("crazyCards", ['ui.router']);
 
-module.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/Home");
     $stateProvider
     .state('Home', {
@@ -19,9 +19,33 @@ module.config(function ($stateProvider, $urlRouterProvider) {
          url: "/Portfolio",
          templateUrl: "/Angular/Views/portfolio.html"
      })
+        .state('Cards', {
+            url: "/Cards",
+            templateUrl: "/Angular/Views/Cards.html",
+            controller: function ($scope) {
+                //$scope.$watch('customer.DOB', function () {
+                //    if ($scope.customer != null) {
+                //        if ($scope.customer.DOB != null) {
+                //            $scope.calculateAge($scope.customer.DOB)
+                //        }
+                //    }
+                 
+                //});
+
+                //$scope.calculateAge = function calculateAge(birthday) { // birthday is a date
+                //    if (isNaN(birthday.getTime())) {  // d.valueOf() could also work
+                //        // date is not valid
+                //        return "";
+                //    }
+                //    var ageDifMs = Date.now() - birthday.getTime();
+                //    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+                //    return Math.abs(ageDate.getUTCFullYear() - 1970);
+                //}
+            }
+        })
     ;
 });
-module.run(function ($rootScope) {
+app.run(function ($rootScope) {
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
         console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
     });
@@ -43,4 +67,31 @@ module.run(function ($rootScope) {
         console.log('$stateNotFound ' + unfoundState.to + '  - fired when a state cannot be found by its name.');
         console.log(unfoundState, fromState, fromParams);
     });
+});
+app.filter('ageFilter', function () {
+    function calculateAge(birthday) { // birthday is a date
+        var ageDifMs = Date.now() - new Date(birthday).getTime();
+        var ageDate = new Date(ageDifMs); // miliseconds from epoch
+        return Math.abs(ageDate.getUTCFullYear() - 1970) ;
+    }
+    function monthDiff(d1, d2) {
+        if (d1 < d2) {
+            var months = d2.getMonth() - d1.getMonth();
+            return months <= 0 ? 0 : months;
+        }
+        return 0;
+    }
+    return function (birthdate) {
+        if (birthdate == undefined) {
+            return;
+        }
+        if (NaN == Date.parse(birthdate)) {
+            return;
+        }
+
+        var age = calculateAge(birthdate);
+        if (age == 0)
+            return monthDiff(birthdate, new Date()) + ' Months';
+        return age;
+    };
 });
