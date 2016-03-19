6695aea3c3ee459890860f928df891e155c25cc5
pnvpratik <pnvoratik@gmail.com>
2016-03-19 14:14:53 +0000

    UI-router working

--- Crazy.Cards.Web/Angular/Module/crazyCards.js
+++ Crazy.Cards.Web/Angular/Module/crazyCards.js
@@ -1,1 +1,46 @@
-﻿var module = angular.module("crazyCards", []);
+﻿var module = angular.module("crazyCards", ['ui.router']);
+
+module.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/Home");
+    $stateProvider
+    .state('Home', {
+        url: "/Home",
+        templateUrl: "/Angular/Views/Home.html"
+    })
+    .state('About', {
+        url: "/About",
+        templateUrl: "/Angular/Views/About.html"
+    })
+    .state('Contact', {
+        url: "/Contact",
+        templateUrl: "/Angular/Views/Contact.html"
+    })
+     .state('Portfolio', {
+         url: "/Portfolio",
+         templateUrl: "/Angular/Views/portfolio.html"
+     })
+    ;
+});
+module.run(function ($rootScope) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+        console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
+    });
+
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams) {
+        console.log('$stateChangeError - fired when an error occurs during transition.');
+        console.log(arguments);
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        console.log('$stateChangeSuccess to ' + toState.name + '- fired once the state transition is complete.');
+    });
+
+    $rootScope.$on('$viewContentLoaded', function (event) {
+        console.log('$viewContentLoaded - fired after dom rendered', event);
+    });
+
+    $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+        console.log('$stateNotFound ' + unfoundState.to + '  - fired when a state cannot be found by its name.');
+        console.log(unfoundState, fromState, fromParams);
+    });
+});
--- Crazy.Cards.Web/Scripts/freelancer.js
+++ Crazy.Cards.Web/Scripts/freelancer.js
@@ -5,15 +5,15 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+//$(function() {
+//    $('body').on('click', '.page-scroll a', function(event) {
+//        var $anchor = $(this);
+//        $('html, body').stop().animate({
+//            scrollTop: $($anchor.attr('href')).offset().top
+//        }, 1500, 'easeInOutExpo');
+//        event.preventDefault();
+//    });
+//});
 
 // Floating label headings for the contact form
 $(function() {
@@ -26,10 +26,10 @@ $(function() {
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+//// Highlight the top nav as scrolling occurs
+//$('body').scrollspy({
+//    target: '.navbar-fixed-top'
+//})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
