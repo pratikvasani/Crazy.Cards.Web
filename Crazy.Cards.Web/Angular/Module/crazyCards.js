6c39d70a33fb249c9e2cf5e1ba8d1e5d93b4356e
pnvpratik <pnvpratik@gmail.com>
2016-03-24 00:53:17 +0000

    Scroll fix.

--- Crazy.Cards.Web/Angular/Module/crazyCards.js
+++ Crazy.Cards.Web/Angular/Module/crazyCards.js
@@ -73,3 +73,8 @@ app.service('getOffersService', ['$http', function ($http) {
     }
 
 }]);
+app.run(function ($rootScope) {
+$rootScope.$on('$stateChangeSuccess', function() {
+   document.body.scrollTop = document.documentElement.scrollTop = 0;
+});
+});
