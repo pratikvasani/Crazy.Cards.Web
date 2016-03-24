d4acce233c3580d87f0c648bebb79653a2feeef6
pnvpratik <pnvpratik@gmail.com>
2016-03-24 01:18:22 +0000

    Structuremap

--- Crazy.Cards.Web/Controllers/CardOffersController.cs
+++ Crazy.Cards.Web/Controllers/CardOffersController.cs
@@ -1,5 +1,4 @@
 ﻿
-using Crazy.Cards.Repository;
 using Crazy.Cards.Web.Models;
 using System;
 using System.Collections.Generic;
@@ -8,57 +7,33 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Crazy.Cards.Core.Models;
+using Crazy.Cards.Core;
 
 namespace Crazy.Cards.Web.Controllers
 {
     public class CardOffersController : ApiController
     {
+        private readonly ICardsRepository repository;
+        public CardOffersController(ICardsRepository repo)
+        {
+            this.repository = repo;
+        }
         // GET api/values
         [Route("api/Offers")]
         public IEnumerable<OfferViewModel> PostOffers(CustomerViewModel model)
         {
-            InMemoryCardsRepository r = new InMemoryCardsRepository();
-            var cardOffers =  r.GetCards(model.EmploymentStatus, model.AnnualIncome);
+            var cardOffers =  repository.GetCards(model.EmploymentStatus, model.AnnualIncome);
             return Map(cardOffers);
-            //OfferViewModel offer = new OfferViewModel();
-            //offer.CardName = "Anywhere Card";
-            //offer.CardDescription = "The card available to anyone , anywhere.";
-            //offer.CardAPR = 33.9M;
-            //offer.BalanceTransferOfferDuration = 0;
-            //offer.PurchaseOfferDuration = 0;
-            //offer.CreditLimit = 300;
-            //offer.CardImage = "/img/portfolio/cake.png";
-
-            //OfferViewModel offerStudent = new OfferViewModel();
-            //offerStudent.CardName = "Student life credit card";
-            //offerStudent.CardDescription = "The student life credit card is specially for students.";
-            //offerStudent.CardAPR = 18.9M;
-            //offerStudent.BalanceTransferOfferDuration = 0;
-            //offerStudent.PurchaseOfferDuration = 6;
-            //offerStudent.CreditLimit = 1200;
-            //offerStudent.CardImage = "/img/portfolio/circus.png";
-
-            //OfferViewModel offerEmp = new OfferViewModel();
-            //offerEmp.CardName = "Liquid card";
-            //offerEmp.CardDescription = "For the working professional.";
-            //offerEmp.CardAPR = 33.3M;
-            //offerEmp.BalanceTransferOfferDuration = 12;
-            //offerEmp.PurchaseOfferDuration = 6;
-            //offerEmp.CreditLimit = 3000;
-            //offerEmp.CardImage = "/img/portfolio/safe.png";
-
-
-
-           // return new List<OfferViewModel>() { offer ,offerStudent , offerEmp };
+            
 
         }
 
         private IEnumerable<OfferViewModel> Map(IEnumerable<CreditCard> cardOffers)
         {
-            List<OfferViewModel> a = new List<OfferViewModel>();
+            List<OfferViewModel> viewModelList = new List<OfferViewModel>();
             foreach (var r in cardOffers)
             {
-                a.Add(new OfferViewModel() {
+                viewModelList.Add(new OfferViewModel() {
                      BalanceTransferOfferDuration =r.BalanceTransferOfferDuration,
                       CardAPR = r.APR,
                       CardDescription =r.CardDescription,
@@ -71,29 +46,10 @@ namespace Crazy.Cards.Web.Controllers
 
             }
 
-            return a;
+            return viewModelList;
             
         }
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+       
     }
 }
